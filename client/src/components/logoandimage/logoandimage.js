ab413ee734e438562d8e0ae5cee16b7aa75e7e90
mcruz <mcruzpr86@gmail.com>
2020-09-16 16:53:12 -0400

    style title

--- client/src/components/logoandimage/logoandimage.js
+++ client/src/components/logoandimage/logoandimage.js
@@ -4,8 +4,16 @@ import "./logoandimage.css"
 function Logo() {
 
     return (
+        <div>
         <div> 
-            <a href="/" className='logoTitle' style={{textDecoration:'none', color: 'maroon', fontSize: '80px'}}>The Top Shelf</a>
+            <a href="/" className='logoTitle' style={{textDecoration:'none', color: 'maroon', fontSize: '80px', marginLeft: '-180px'}}>The Top Shelf</a>
+            
+        </div>
+        <div>
+            <a href="/" className='logoTitle' style={{textDecoration:'none', color: 'maroon', fontSize: '30px'}}>Your virtual happy hour</a>
+
+        </div>
+
         </div>
     )
 }
